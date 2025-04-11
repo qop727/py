c33c62f0f15fc4784fbe5fa6b7e2fc6c081f256f
Matěj H <qop727@gmail.com>
2025-04-11 19:22:02 +0200

    bash to run graphical

---
+++ bash_to_run_graphical.sh
@@ -0,0 +1,5 @@
+# Usage of actual interpreter outside of VSC is needed.
+
+python3 /home/l_qopd/qop/GitHub/py/course_kitner/du6/project_calc_graphical.py     # Or diferent path.
+
+python3 /home/l_qopd/qop/GitHub/py/py_games/py_snake.py     # Or diferent path.
