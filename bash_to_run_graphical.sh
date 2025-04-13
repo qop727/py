b0c6fa05ef44ca336eeb1ae5b36506b928fae10c
Matěj H <qop727@gmail.com>
2025-04-13 11:51:47 +0200

    bash to change execute permissions

--- bash_to_run_graphical.sh
+++
@@ -1,5 +0,0 @@
-# Usage of actual interpreter outside of VSC is needed.
-
-python3 /home/l_qopd/qop/GitHub/py/course_kitner/du6/project_calc_graphical.py     # Or different path.
-
-python3 /home/l_qopd/qop/GitHub/py/py_games/py_snake.py     # Or different path.
